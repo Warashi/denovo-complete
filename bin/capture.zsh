cee6df9680745fa31913a6bf29a33824c3022309
Shinnosuke Sawada <6warashi9@gmail.com>
2024-02-23 15:14:47 +0900

    style(bin): format capture shell

--- bin/capture.zsh
+++ bin/capture.zsh
@@ -10,13 +10,13 @@ zpty z zsh -f -i
 local line
 
 () {
-    zpty -w z source $1
-    repeat 4; do
-        zpty -r z line
-        [[ $line == ok* ]] && return
-    done
-    echo 'error initializing.' >&2
-    exit 2
+	zpty -w z source $1
+	repeat 4; do
+		zpty -r z line
+		[[ $line == ok* ]] && return
+	done
+	echo 'error initializing.' >&2
+	exit 2
 } =(<$script_dir/capture-internal.zsh)
 
 zpty -w z "$*"$'\t'
@@ -24,11 +24,11 @@ zpty -w z "$*"$'\t'
 integer tog=0
 # read from the pty, and parse linewise
 while zpty -r z; do :; done | while IFS= read -r line; do
-    if [[ $line == *$'\0\r' ]]; then
-        (( tog++ )) && return 0 || continue
-    fi
-    # display between toggles
-    (( tog )) && echo -E - $line
+	if [[ $line == *$'\0\r' ]]; then
+		(( tog++ )) && return 0 || continue
+	fi
+	# display between toggles
+	(( tog )) && echo -E - $line
 done
 
 return 2
